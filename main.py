b101f3d232b5c6055d6b1d67bcb58cf95e378cb2
Gouravsharma20 <gouravsharma20a@gmail.com>
2025-03-30 01:16:41 +0530

    Code is running correctly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from routers import customer_routes, trainer_routes, owner_routes
 from database import engine, Base
+from fastapi.responses import PlainTextResponse
 
 # Creating all database tables
 Base.metadata.create_all(bind=engine)
@@ -12,7 +13,8 @@ app.include_router(customer_routes.router, prefix="/customer", tags=["Customer"]
 app.include_router(trainer_routes.router, prefix="/trainer", tags=["Trainer"])
 app.include_router(owner_routes.router, prefix="/owner", tags=["Owner"])
 
-@app.get("/")
+@app.get("/",response_class=PlainTextResponse)
 def home():
-    return {"message": "Welcome to the Fitness Management System"}
+    return "**# Welcome to the Fitness Management System**"
+    
 
